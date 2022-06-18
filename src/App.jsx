a609c1dbe88fcd17019c78af411d2e14c117bace
Nikolay Efremov <efrem_nn@mail.ru>
2022-06-19 03:33:44 +0700

    lesson3 DZ

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,117 @@
-import React, {useEffect, useState} from "react";
-import './App.scss'
-import Message from "./components/Message"
+import React, {useEffect, useState} from 'react';
+import clsx from 'clsx';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ListChat from './components/ListChat';
+import Message from "./components/Message";
 import Form from "./components/Form";
 
-function App() {
-    // id,text,author
-    const [id, setId] = useState(1)
+import {useStylesApp} from "./styles"
+
+export default function App() {
+    const classes = useStylesApp();
+    const [open, setOpen] = React.useState(true);
     const [messageList, setMessageList] = useState([])
-    const [msgCount, setMsgCount] = useState(0)
-
-
-    function handClick (text, author) {
-        setMessageList([...messageList, {id, text, author}])
-        setId(id => id + 1)
-        setMsgCount(count => count + 1)
-    }
 
     useEffect(() => {
         setTimeout(() => {
-            setMessageList([...messageList, {id, text: "hello fix message Robot", author: "Robot"}])
-            setId(id => id + 1)
+            botMessage(messageList)
         }, 1500)
-    },[msgCount])
+    }, [messageList])
+
+
+    const handClick = (text, author) => {
+        if (text !== '' && author !== '') {
+            setMessageList([...messageList, {id: Date.now(), text, author}])
+        }
+    }
+
+    const botMessage = (messageList) => {
+        const lastAuthor = messageList[messageList.length - 1]
+        if (lastAuthor && lastAuthor.author) {
+            setMessageList(prevState => [...prevState, {
+                id: Date.now(),
+                text: `сообщение отправленно от ${lastAuthor.author}`
+            }])
+        }
+    }
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
     return (
-        <div className="App">
-            <div>
-                {messageList.map((msg => <Message key={msg.id} message={msg} />))}
-            </div>
-            <div className="App_input">
-                <Form handClick={handClick}/>
-            </div>
+        <div className={classes.root}>
+            <CssBaseline/>
+            <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+                <Toolbar className={classes.toolbar}>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                        Мессенджер
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                classes={{
+                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+                }}
+                open={open}
+            >
+                <div className={classes.toolbarIcon}>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon/>
+                    </IconButton>
+                </div>
+                <Divider/>
+                <List>
+                    <ListChat/>
+                </List>
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.appBarSpacer}/>
+                <Container maxWidth="lg" className={classes.container}>
+                    <Paper elevation={5} style={{height: '70vh'}} className={classes.paper}>
+                        <Grid container spacing={3}>
+                            <Grid md={12} item>
+                                {messageList.map((msg => <Message key={msg.id} message={msg}/>))}
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Container>
+                <Container maxWidth="lg">
+                    <Paper elevation={5} className={classes.paper}>
+                        <Grid md={12} item>
+                            <Grid container direction={'row'} justifyContent={'center'} alignContent={'center'}
+                                  spacing={3}>
+                                <Form handClick={handClick}/>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Container>
+            </main>
         </div>
-      );
+    );
 }
-
-export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,91 @@
-import React, {useState} from 'react';
+import React, {useCallback, useRef, useState} from 'react';
+import {TextField, Button, FormControl} from "@material-ui/core";
+import {Send} from '@material-ui/icons';
+import Grid from "@material-ui/core/Grid";
+
+import { useStylesForm } from "../styles"
 
 const Form = ({handClick}) => {
 
+    const classes = useStylesForm()
     const [text, setText] = useState('')
     const [author, setAuthor] = useState('')
+    const input = useRef(null)
 
-    const handText = (e) => {
+    const handText = useCallback((e) => {
         setText(e.target.value)
-    }
+    }, [])
 
-    const handAuthor = (e) => {
+    const handAuthor = useCallback((e) => {
         setAuthor(e.target.value)
+    }, [])
+
+    const handForm = (e) => {
+        e.preventDefault()
+        handClick(text, author)
+        setText('')
+        input.current.focus()
     }
 
-    const handForm = () => {
-        handClick(text, author)
-    }
+    const handleChangeForm = useCallback((e) => {
+        if(e.key === 'Enter') handClick(text, author)
+    }, [text, author, handClick])
 
     return (
-        <div>
-            <div>
-                <label htmlFor="text">сообщения: </label>
-                <input onChange={handText} value={text} id="text" name="text" type="text"/>
-            </div>
-            <div>
-                <label htmlFor="author">автор: </label>
-                <input onChange={handAuthor} value={author} id="author" name="author" type="text"/>
-            </div>
-            <button onClick={handForm}>отправить</button>
-        </div>
+        <>
+            <Grid md={3} item>
+                <FormControl fullWidth>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        autoFocus
+                        id="outlined-basic"
+                        label="автор"
+                        name="author"
+                        value={author}
+                        onChange={handAuthor}
+                        onKeyDown={handleChangeForm}
+                        placeholder="Автор сообщения..."
+                    />
+                </FormControl>
+            </Grid>
+
+            <Grid md={7} item>
+                <FormControl fullWidth>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="text"
+                        label="сообщения"
+                        name="text"
+                        value={text}
+                        inputRef={input}
+                        onChange={handText}
+                        onKeyDown={handleChangeForm}
+                        placeholder="Текст сообщения..."
+                    />
+                </FormControl>
+            </Grid>
+            <Grid md={2} item>
+                <FormControl fullWidth>
+                    <Button
+                        onClick={handForm}
+                        endIcon={<Send />}
+                        type="submit"
+                        fullWidth
+                        variant="text"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        отправить
+                    </Button>
+                </FormControl>
+            </Grid>
+        </>
     );
 };
 
